7a5641fc103dc3ad8627a92b7c07252177649072
Lukacs Alex Szucs <szucs.lukacs.alex@gmail.com>
2025-06-02 01:11:03 +0200

    feat: add api folder and entry point
    Add folder to contain the implementation of the task and a default main file to serve as an entry point.

---
+++ task-management-api/main.py
@@ -0,0 +1,9 @@
+"""This file serves as the entry point for the Task Management API."""
+
+
+def main():
+    """Main entry point for the Task Management API application."""
+
+
+if __name__ == "__main__":
+    main()
