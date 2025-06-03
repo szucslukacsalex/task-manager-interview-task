e819a91fea3606c85747bfb0e220236b58da069a
Lukacs Alex Szucs <szucs.lukacs.alex@gmail.com>
2025-06-03 21:54:20 +0200

    feat: modify main to handle direct start

--- task-management-api/app/main.py
+++ task-management-api/app/main.py
@@ -1,8 +1,20 @@
 """This file serves as the entry point for the Task Management API."""
 
+import os
+import sys
+from pathlib import Path
+
+import uvicorn
+
 
 def main():
     """Main entry point for the Task Management API application."""
+
+    file_path = Path(__file__).resolve()
+    api_root = file_path.parent.parent
+    os.chdir(api_root)
+    sys.path.insert(0, str(api_root))
+    uvicorn.run("app.api.api:app", reload=True)
 
 
 if __name__ == "__main__":
